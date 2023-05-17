537c87f52a346aa00cbb46bda866a09ab01538ad
kasperis3 <kushaal@berkeley.edu>
2023-05-17 16:23:19 -0700

    ts up and running

--- src/Game.tsx
+++ src/Game.tsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import SetUp from "./components/SetUp";
 
 function Game() {
-  const [count, setCount] = useState(0);
+  const [numPlayers, setNumPlayers] = useState(0);
 
   return (
     <>
-      <SetUp />
+      {numPlayers === 0 ? (
+        <SetUp setNumPlayers={setNumPlayers} />
+      ) : (
+        <div>begin game for {numPlayers} players</div>
+      )}
     </>
   );
 }
--- src/components/SetUp.tsx
+++ src/components/SetUp.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Dispatch, SetStateAction, ChangeEvent } from "react";
 
-function SetUp() {
-  return <div>SetUp</div>;
+export type Dispatcher<S> = Dispatch<SetStateAction<S>>;
+
+interface IProps {
+  setNumPlayers: Dispatcher<number>;
 }
+
+const SetUp = (props: IProps) => {
+  const handleNumPlayers = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    props.setNumPlayers(+e.target.value);
+  };
+
+  return (
+    <>
+      Enter number players:{" "}
+      <input onChange={handleNumPlayers} type="text" pattern="\d+" />
+    </>
+  );
+};
 
 export default SetUp;
