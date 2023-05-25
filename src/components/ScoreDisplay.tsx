f698df829e35cedfe143658f5e41c4db22493677
kasperis3 <kushaal@berkeley.edu>
2023-05-25 16:49:53 -0700

    update type imports

--- src/components/ScoreDisplay.tsx
+++ src/components/ScoreDisplay.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Row } from "../Game";
+import { Row } from "../utils/types";
 import { basisTypes } from "../utils/basis";
 
 interface iProps {
