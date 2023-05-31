441905db3097d7c3329d14ca9655f1362ac1fd28
kasperis3 <kushaal@berkeley.edu>
2023-05-31 11:49:48 -0700

    fix undefined in Header test

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,11 +1,10 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
-// import { basisTypes } from "../utils/basis";
 
 describe("Header", () => {
   it("renders Header", () => {
-    render(<Header numPlayers={3} />);
+    render(<Header numPlayers={4} />);
     screen.debug();
   });
 });
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,19 +8,15 @@ interface iProps {
 }
 
 function Header(props: iProps) {
-  console.log(basisTypes[props.numPlayers]);
   const basis = basisTypes[props.numPlayers];
   const playerNames = playersNames.slice(0, props.numPlayers);
 
   return (
     <>
-      {/* <div>
-        This is b4sis {basis} player names {playerNames}
-      </div> */}
       <div className={`${basis} text-center`}>Hand</div>
       {playerNames.map((name, index) => {
         return (
-          <div className={`${basis} grow-0 shrink-0 text-center`}>
+          <div key={index} className={`${basis} grow-0 shrink-0 text-center`}>
             <input
               className="w-full"
               placeholder={name}
