bfb0b1ef7b8f9fdf314ad479de5b13d467e2b6f7
kasperis3 <kushaal@berkeley.edu>
2023-06-01 16:50:14 -0700

    test for Header complete, check num players and about name change

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,11 +1,27 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
+import userEvent from "@testing-library/user-event";
 
 describe("Header", () => {
-  it("renders Header", () => {
-    const utils = render(<Header numPlayers={4} />);
-    screen.debug();
+  it("renders Header for 4 players", () => {
+    render(<Header numPlayers={4} />);
+    expect(screen.getByText("Hand")).toBeInTheDocument();
     const inputs = screen.getAllByRole("textbox");
+    expect(inputs).toHaveLength(4);
+  });
+
+  it("renders Header for 6 players", () => {
+    render(<Header numPlayers={6} />);
+    expect(screen.getByText("Hand")).toBeInTheDocument();
+    const inputs = screen.getAllByRole("textbox");
+    expect(inputs).toHaveLength(6);
+  });
+
+  it("name change for player 1", async () => {
+    render(<Header numPlayers={6} />);
+    const user = userEvent.setup();
+    await user.type(screen.getAllByRole("textbox")[0], "Player One");
+    expect(screen.getAllByRole("textbox")[0]).toHaveTextContent("");
   });
 });
